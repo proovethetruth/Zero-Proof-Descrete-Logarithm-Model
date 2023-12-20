f17c2d6deb11dd296262fb4389456cbd923b4ffc
seeker <populpock2@gmail.com>
2023-12-21 00:02:12 +0300

    Now it works

--- main.py
+++ main.py
@@ -1,67 +1,79 @@
 import random
- 
-class ZeroProofProtocol:
-    def __init__(self, p: int, g: int, a: int, b: int):
+
+class Prover:
+    def __init__(self, p, A, x):
         self.p = p
-        self.g = g
-        self.a = a
-        self.b = b
- 
-    def calculate_public_keys(self):
-        A = pow(self.g, self.a, self.p)
-        B = pow(self.g, self.b, self.p)
+        self.A = A
+        self.x = x
 
-        return A, B
- 
-    def generate_challenge(self):
-        challenge = random.randint(1, self.p - 1)
- 
-        return challenge
- 
-    def verify_proof(self, A: int, B: int, challenge: int):
-        a_prime = (self.a + challenge) % (self.p - 1)
-        b_prime = (self.b + challenge) % (self.p - 1)
- 
-        if pow(A, b_prime, self.p) == pow(B, a_prime, self.p):
-            return True
-        else:
-            return False
-        
+    def generate_proof(self):
+        self.r = random.randint(0, self.p - 2)
+        h = pow(self.A, self.r, self.p)
+        return h
 
-def calculate_false_proof_probability(protocol, A, B, num_attempts=1000):
-        success_count = 0
+    def respond_to_challenge(self, b):
+        s = (self.r + (b * self.x)) % (self.p - 1)
+        return s
 
-        for _ in range(num_attempts):
-            challenge_attempt = random.randint(1, protocol.p - 1)
+class Verifier:
+    def __init__(self, p, A, B):
+        self.p = p
+        self.A = A
+        self.B = B
 
-            a_prime_attempt = (protocol.a + challenge_attempt) % (protocol.p - 1)
-            b_prime_attempt = (protocol.b + challenge_attempt) % (protocol.p - 1)
+    def challenge_prover(self):
+        b = random.getrandbits(1)
+        return b
 
-            if pow(A, b_prime_attempt, protocol.p) == pow(B, a_prime_attempt, protocol.p):
-                success_count += 1
+    def verify_proof(self, h, s):
+        left_side = pow(self.A, s, self.p)
+        right_side = (h * pow(self.B, self.b, self.p)) % self.p
+        return left_side == right_side
 
-        probability = success_count / num_attempts
-        return probability
+class Attacker:
+    def __init__(self, p, A, B):
+        self.p = p
+        self.A = A
+        self.B = B
+
+    def generate_fake_proof(self, potential_x):
+        r_fake = random.randint(0, self.p - 2)
+        h_fake = pow(self.A, r_fake, self.p)
+        b_fake = random.getrandbits(1)
+        s_fake = (r_fake + (b_fake * potential_x)) % (self.p - 1)
+        return h_fake, s_fake
 
 
 if __name__ == "__main__":
-    p_value = 2048
-    g_value = 5
-    a_value = 1081
-    b_value = 1708
-    
-    protocol = ZeroProofProtocol(p_value, g_value, a_value, b_value)
-    A_value, B_value = protocol.calculate_public_keys()
+    p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381FFFFFFFFFFFFFFFF
+    A = 2
+    x = 6
+    B = pow(A, x, p)
 
-    false_proof_probability = calculate_false_proof_probability(protocol, A_value, B_value)
-    print(f"The probability of a successful false proof: {false_proof_probability}")
+    prover = Prover(p, A, x)
+    verifier = Verifier(p, A, B)
 
-    print(f"The public key for Alice is {A_value}.")
-    print(f"The public key for Bob is {B_value}.")
-    
-    challenge_value = protocol.generate_challenge()
-    proof_valid = protocol.verify_proof(A_value, B_value, challenge_value)
-    if proof_valid:
-        print("The proof is valid.\n")
-    else:
-        print("The proof is not valid.\n")
+    h = prover.generate_proof()
+    verifier.b = verifier.challenge_prover()
+    s = prover.respond_to_challenge(verifier.b)
+    result_prover = verifier.verify_proof(h, s)
+    print("Prover Zero-Knowledge Proof Verification Result:", result_prover)
+
+    # - - - - - - - - - - - ATTACK  - - - - - - - - - - - #
+
+    attacker = Attacker(p, A, B)
+
+    potential_x = 0
+    max_attempts = 1000
+
+    count_success = 0
+    for _ in range(max_attempts):
+        fake_h, fake_s = attacker.generate_fake_proof(potential_x)
+        result_attacker = verifier.verify_proof(fake_h, fake_s)
+
+        if result_attacker:
+            count_success += 1
+
+        potential_x += 1
+
+    print(f"Probability of successful attack: {count_success / max_attempts * 100} %")
